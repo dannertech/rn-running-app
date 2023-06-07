993a3be8cd2f0d9a4cda8d1957d1e3767c7ad7f9
dannertech <danner@danner.tech>
2023-06-07 18:44:01 -0400

    added map to track screen

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://5241-2601-100-897e-5cb0-dd2-4704-1afd-4a3a.ngrok-free.app'
+    baseURL: 'https://57a3-2601-100-897e-5cb0-7457-cf90-a9ce-d88d.ngrok-free.app'
 });
--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import {StyleSheet, View, Text, Button} from 'react-native';
-import {SafeAreaView} from 'react-native-safe-area-context';
+import {StyleSheet, Button} from 'react-native';
+import {Text} from 'react-native-elements';
+import {SafeAreaView} from 'react-navigation';
+import Map from '../components/Map';
 
 const TrackCreateScreen = () => {
     return(
-        <SafeAreaView>
-            <Text>This is the Track Create Screen</Text>
+        <SafeAreaView forceInset={{top: 'always'}}>
+            <Text h2>This is the Track Create Screen</Text>
+            <Map />
         </SafeAreaView>
     )
 };
