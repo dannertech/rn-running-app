dc7da93a9d3a52d33a25d68327c07056ff7c57a2
dannertech <danner@danner.tech>
2023-06-08 23:15:00 -0400

    added some functionality to request permissions from user

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {StyleSheet, Button} from 'react-native';
 import {Text} from 'react-native-elements';
 import {SafeAreaView} from 'react-navigation';
 import Map from '../components/Map';
+import {requestForegroundPermissionsAsync} from 'expo-location';
 
 const TrackCreateScreen = () => {
+    const [error, setError] = useState(null);
+    const startWatching = async () => {
+        try {
+            const { granted } = await requestForegroundPermissionsAsync();
+        }catch (error) {
+            setError(error);
+        }
+    }
+
+    useEffect(() => {
+        startWatching();
+    },[])
     return(
         <SafeAreaView forceInset={{top: 'always'}}>
             <Text h2>This is the Track Create Screen</Text>
             <Map />
+            {error ? <Text>Please enable Location Services</Text> : null}
         </SafeAreaView>
     )
 };
