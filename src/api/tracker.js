7f6a749b1076dd5eea81f2fa100ab5c68ba9a321
dannertech <danner@danner.tech>
2023-06-10 10:48:33 -0400

    added fake location code

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://57a3-2601-100-897e-5cb0-7457-cf90-a9ce-d88d.ngrok-free.app'
+    baseURL: 'https://0846-2601-100-897e-5cb0-bcd2-97c7-f396-b458.ngrok-free.app'
 });
